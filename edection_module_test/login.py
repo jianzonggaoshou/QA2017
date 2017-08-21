3da5c0f2ad21f30bd99d1f3044894a1e6074df23
xuzhen <xuzhen0913@163.com>
2017-08-21 10:08:18 +0800

    8.20

--- edection_module_test/login.py
+++ edection_module_test/login.py
@@ -5,20 +5,20 @@ from time import sleep
 
 
 class Login(unittest.TestCase):
-	def setUp(self):
-		print "test start"
+    def setUp(self):
+        print "test start"
 
-	def tearDown(self):
-		print "test end"
+    def tearDown(self):
+        print "test end"
 
-	def login(self):
-		browser = webdriver.Chrome()
-		browser.maximize_window()
+    def login(self):
+        browser = webdriver.Chrome()
+        browser.maximize_window()
 
-		browser.get("http://192.168.8.88:8080/sito2000/login.jsp")
-		browser.find_element_by_id("userId").send_keys("15609100803")
-		browser.find_element_by_id("password").send_keys("123456")
-		browser.find_element_by_xpath("/html/body/div/div[2]/div/div[2]/button").click()
+        browser.get("http://192.168.8.88:8080/sito2000/login.jsp")
+        browser.find_element_by_id("userId").send_keys("15609100803")
+        browser.find_element_by_id("password").send_keys("123456")
+        browser.find_element_by_xpath("/html/body/div/div[2]/div/div[2]/button").click()
 
-	if __name__ == '__main__':
-		unittest.main()
+    if __name__ == '__main__':
+        unittest.main()
